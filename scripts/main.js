4663f3a6168bd77f26ef5ba1ab9d471f6a3aeccf
CuriousChronicles <jayti.pattni@gmail.com>
2026-03-07 03:33:15 +1300

    created toggle theme of website

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,24 @@
 const sidebar = document.getElementById('sidebar');
+const themeSwitch = document.getElementById('theme-switch');
+let darkMode = localStorage.getItem('darkMode');
 
 function toggleSidebar() {
     sidebar.classList.toggle('close');
 }
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'active');
+    darkMode = "active";
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null);
+    darkMode = "inactive";
+}
+
+themeSwitch.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    darkMode !== "active" ? enableDarkMode() : disableDarkMode();
+});
